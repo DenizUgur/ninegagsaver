9896859cb4f5d94adc0fcfce4626ddada547f11b
DenizUgur <deniz343@gmail.com>
2015-12-09 20:35:25 +0200

    update

--- app/src/main/java/com/denizugur/ninegagsaver/WriteObjectSP.java
+++ app/src/main/java/com/denizugur/ninegagsaver/WriteObjectSP.java
@@ -1,35 +1,23 @@
 package com.denizugur.ninegagsaver;
 
-import java.util.List;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
-import com.google.gson.Gson;
-import org.json.JSONObject;
-/**
- * Created by deniz on 28/08/15.
- */
 
-/**
- * Allows the reading/writing of an object from/to a file
- * inside the application's /data directory. Allows for
- * more efficient storing of objects such as arrays instead
- * of within SharedPreferences
- *
- * @author Isaac Whitfield
- * @version 31/07/2013
- */
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
 public class WriteObjectSP {
 
     public static final String GAGS = "com.denizugur.ninegagsaver.gags";
     private Context parent;
-    private List<String> list;
 
-    public WriteObjectSP(Context c){
+    public WriteObjectSP(Context c) {
         parent = c;
     }
 
-    public void writeObject(JSONObject inputObject, String id){
+    public void writeObject(JSONObject inputObject, String id) {
         SharedPreferences.Editor prefsEditor = parent.getSharedPreferences(GAGS, Context.MODE_PRIVATE).edit();
 
         Gson gson = new Gson();
