7345c2aa90f8c5158d4c10529f59878df27891cf
DenizUgur <deniz343@gmail.com>
2015-12-09 20:30:28 +0200

    update

--- app/src/main/java/com/denizugur/ninegagsaver/gagInfo.java
+++ app/src/main/java/com/denizugur/ninegagsaver/gagInfo.java
@@ -11,18 +11,23 @@ public class gagInfo {
     public void setPhotoId(String str) {
         this.photoId = str;
     }
+
     public void setTitle(String str) {
         this.title = str;
     }
+
     public void setSaved_Date(String str) {
         this.saved_date = str;
     }
+
     public void setLikes(String str) {
         this.likes = str;
     }
+
     public void setComments(String str) {
         this.comments = str;
     }
+
     public void setFile_Path(String str) {
         this.file_path = str;
     }
